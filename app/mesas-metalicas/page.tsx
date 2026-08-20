e1e9064b0313b34e6b438034fcadc42ed4fca71b
FelipeValgreen <filipo.valverde@gmail.com>
2026-08-20 18:24:55 -0400

    feat: add mesas metalicas landing

---
+++ app/mesas-metalicas/page.tsx
@@ -0,0 +1,3 @@
+import {CommercialExpansionPage,getCommercialExpansionMetadata} from "@/components/CommercialExpansionPage";
+export const metadata=getCommercialExpansionMetadata("/mesas-metalicas");
+export default function Page(){return <CommercialExpansionPage slug="/mesas-metalicas"/>}
