7b251e9f6363977756974247e173aee99b76f921
FelipeValgreen <filipo.valverde@gmail.com>
2026-08-20 18:24:39 -0400

    feat: add camas metalicas landing

---
+++ app/camas-metalicas/page.tsx
@@ -0,0 +1,3 @@
+import {CommercialExpansionPage,getCommercialExpansionMetadata} from "@/components/CommercialExpansionPage";
+export const metadata=getCommercialExpansionMetadata("/camas-metalicas");
+export default function Page(){return <CommercialExpansionPage slug="/camas-metalicas"/>}
