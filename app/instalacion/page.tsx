c2fc93547e478c1fa937a94bcb81d92f414e61a6
FelipeValgreen <filipo.valverde@gmail.com>
2026-08-20 18:25:26 -0400

    feat: add instalacion landing

---
+++ app/instalacion/page.tsx
@@ -0,0 +1,3 @@
+import {CommercialExpansionPage,getCommercialExpansionMetadata} from "@/components/CommercialExpansionPage";
+export const metadata=getCommercialExpansionMetadata("/instalacion");
+export default function Page(){return <CommercialExpansionPage slug="/instalacion"/>}
