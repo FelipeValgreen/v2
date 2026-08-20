9ab04aecc8c549683653d49a8416dbf576028fd1
FelipeValgreen <filipo.valverde@gmail.com>
2026-08-20 18:25:10 -0400

    feat: add soldadura mig landing

---
+++ app/soldadura-mig/page.tsx
@@ -0,0 +1,3 @@
+import {CommercialExpansionPage,getCommercialExpansionMetadata} from "@/components/CommercialExpansionPage";
+export const metadata=getCommercialExpansionMetadata("/soldadura-mig");
+export default function Page(){return <CommercialExpansionPage slug="/soldadura-mig"/>}
