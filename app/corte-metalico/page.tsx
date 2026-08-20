ba0843676eb01682cc27d0106d39d8e1875b218b
FelipeValgreen <filipo.valverde@gmail.com>
2026-08-20 18:25:17 -0400

    feat: add corte metalico landing

---
+++ app/corte-metalico/page.tsx
@@ -0,0 +1,3 @@
+import {CommercialExpansionPage,getCommercialExpansionMetadata} from "@/components/CommercialExpansionPage";
+export const metadata=getCommercialExpansionMetadata("/corte-metalico");
+export default function Page(){return <CommercialExpansionPage slug="/corte-metalico"/>}
